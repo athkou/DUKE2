c8dc221560735d64b28da8f64354ca7e3e78ac41
athkou <athkou@hotmail.com>
2016-01-09 10:58:32 +0200

    Added a ToggleGroup and RadioButtons to represent both gender

--- src/module4/Controller.java
+++ src/module4/Controller.java
@@ -2,9 +2,12 @@ package module4;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 
 public class Controller
 {
@@ -13,29 +16,44 @@ public class Controller
     public Controller()
     {
         new_name   = new SimpleStringProperty();
+        message    = new SimpleStringProperty();
+
         baby_birth = new BabyBirths(PATH);
     }
 
     public void HandleSubmitButton(ActionEvent event)
     {
-        String name     = first_name.getText();
-        int year        = Integer.parseInt(birth_year.getText());
-        int new_year    = Integer.parseInt(new_birth_year.getText());
-        String gender   = "f";
+        if(first_name.getText().isEmpty() ||
+           birth_year.getText().isEmpty() ||
+           new_birth_year.getText().isEmpty()) SetMessage("Please fill in all text field with appropriate values. Gender: " + gender);
+        else
+        {
+            String name     = first_name.getText();
+            int year        = Integer.parseInt(birth_year.getText());
+            int new_year    = Integer.parseInt(new_birth_year.getText());
 
-        String new_name = baby_birth.WhatIsNameInYear(name, year, new_year, gender);
-        SetNewName(new_name);
+            String new_name = baby_birth.WhatIsNameInYear(name, year, new_year, gender);
+            SetNewName(new_name);
+        }
     }
 
     public String getNew_name()              { return new_name.get(); }
+    public String getMessage()               { return message.get();  }
+
     public StringProperty new_nameProperty() { return new_name;       }
+    public StringProperty messageProperty()  { return message;        }
 
     public void SetNewName(String new_name) { this.new_name.set(new_name); }
+    public void SetMessage(String message)  { this.message.set(message);   }
 
     @FXML private TextField first_name;
     @FXML private TextField birth_year;
     @FXML private TextField new_birth_year;
+    @FXML private ToggleGroup gender_group;
 
     private StringProperty new_name;
+    private StringProperty message;
     private BabyBirths baby_birth;
+
+    private String gender;
 }
